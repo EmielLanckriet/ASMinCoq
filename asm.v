625df907141ada63368e4a7333072cc633b1b5b3
Emiel Lanckriet <emiel.lanckriet@kuleuven.com>
2024-08-23 18:08:27 +0200

    Added halt and fail and redid the things following from it

--- asm.v
+++ asm.v
@@ -36,11 +36,12 @@ Inductive instr : Type :=
 | Load (dst src: Register)
 | Store (dst: Register) (src : nat + Register)
 | Add (dst: Register) (r1 r2: nat + Register)
-| Sub (dst: Register) (r1 r2: nat + Register).
+| Sub (dst: Register) (r1 r2: nat + Register)
+| Fail
+| Halt.
 
 (**
-| Fail
-| Halt
+
 | Lt (dst: Register) (r1 r2: nat + Register)
 | Add (dst: Register) (r1 r2: nat + Register)
 | Sub (dst: Register) (r1 r2: nat + Register)
@@ -76,6 +77,13 @@ Definition Mem := gmap Addr Word.
 
 Definition ExecConf := (Word * Reg * Mem)%type.
 
+Inductive ConfFlag : Type :=
+(** | Executable  Because code is not part of the memory, we don't need this flag *)
+| Halted
+| Failed
+| NextI.
+
+Definition Conf: Type := ConfFlag * ExecConf.
 
 Definition reg (ϕ : ExecConf) := snd (fst ϕ).
 
@@ -106,125 +114,166 @@ Definition word_from_argument (φ: ExecConf) (src : nat + Register) : option Wor
     | inr r => (reg φ) !! r
     end.
 
-Definition exec_instr_opt (i: instr) (φ: ExecConf) : option ExecConf :=
+Definition exec_instr_opt (i: instr) (φ: ExecConf) : option Conf :=
     match i with
+    | Fail => Some (Failed, φ)
+    | Halt => Some (Halted, φ)
     | Jmp r =>
       wr ← (reg φ) !! r;
-      Some (update_PC φ wr)
+      Some (NextI, update_PC φ wr)
     | Jnz r1 r2 =>
       wr2 ← (reg φ) !! r2;
       wr1 ← (reg φ) !! r1;
       if nonZero wr2 then
-        Some (update_PC φ wr1)
-      else Some (incr_PC φ)
+        Some (NextI, update_PC φ wr1)
+      else Some (NextI, incr_PC φ)
     | Load dst src =>
       wsrc ← (reg φ) !! src;
       asrc ← (mem φ) !! wsrc;
-      Some (incr_PC (update_reg φ dst asrc))
+      Some (NextI, incr_PC (update_reg φ dst asrc))
     | Store dst ρ =>
         wdst ← (reg φ) !! dst;
         tostore ← word_from_argument φ ρ;
-        Some (incr_PC (update_mem φ wdst tostore))
+        Some (NextI, incr_PC (update_mem φ wdst tostore))
     | Mov dst ρ =>
         wdst ← (reg φ) !! dst;
         tomov ← word_from_argument φ ρ;
-        Some (incr_PC (update_reg φ wdst tomov))
+        Some (NextI, incr_PC (update_reg φ wdst tomov))
     | Add dst ρ1 ρ2 =>
         n1 ← word_from_argument φ ρ1;
         n2 ← word_from_argument φ ρ2;
-        Some (incr_PC (update_reg φ dst (n1 + n2)))
+        Some (NextI, incr_PC (update_reg φ dst (n1 + n2)))
     | Sub dst ρ1 ρ2 =>
         n1 ← word_from_argument φ ρ1;
         n2 ← word_from_argument φ ρ2;
-        Some (incr_PC (update_reg φ dst (n1 - n2)))
+        Some (NextI, incr_PC (update_reg φ dst (n1 - n2)))
     end.
+
+Definition exec_instr (i: instr) (φ: ExecConf) : Conf :=
+    match exec_instr_opt i φ with
+    | None => (Failed, φ)
+    | Some conf => conf
+    end.
+
+Lemma exec_instr_opt_exec_instr_some :
+    forall φ i c,
+      exec_instr_opt i φ = Some c →
+      exec_instr i φ = c.
+Proof. unfold exec_instr. by intros * ->. Qed.
+
+Lemma exec_instr_opt_exec_instr_none :
+    forall φ i,
+      exec_instr_opt i φ = None →
+      exec_instr i φ = (Failed, φ).
+  Proof. unfold exec_instr. by intros * ->. Qed.
+
 
 Definition emptyReg : Reg := empty.
 Definition emptyMem : Mem := empty.
 
+(** Contrary to Cerise programs are not part of the memory in this model *)
 Definition program : Type := Word -> option instr.
 
 Definition list_prog_to_prog (li : list instr) : program :=
     fun (w : Word) => nth_error li w.
 
-Definition exec_instr_at_PC (prog : program) (φ : ExecConf) : option ExecConf :=
+Definition exec_step_prog (prog : program) (φ : ExecConf) : Conf :=
     match (prog (PC φ)) with
-    | None => None
-    | Some i => exec_instr_opt i φ
+    | None => (Failed, φ)
+    | Some i => exec_instr i φ
     end.
 
-Fixpoint exec_prog (prog : program) (φ : ExecConf) (time : nat) : ExecConf :=
+Inductive step_prog (prog : program) : Conf -> Conf -> Prop :=
+    | step_PC_fail (φ : ExecConf) (PC_invalid : prog (PC φ) = None) :
+        step_prog prog (NextI, φ) (Failed, φ)
+    | step_PC_succ (φ : ExecConf) (i : instr) (PC_valid : prog (PC φ) = Some i) :
+        step_prog prog (NextI, φ) (exec_instr i φ).
+
+Fixpoint exec_prog (prog : program) (φ : ExecConf) (time : nat) : Conf :=
     match time with
-    | 0 => φ
-    | S time' => match (exec_instr_at_PC prog φ) with
-                 | None => φ
-                 | Some φ' => exec_prog prog φ' time'
+    | 0 => (NextI, φ)
+    | S time' => match (exec_step_prog prog φ) with
+                 | (NextI, φ') => exec_prog prog φ' time'
+                 | x => x
                  end
     end.
 
-Inductive Exec_prog_one_step_ind (prog : program) (φ : ExecConf) : ExecConf -> Prop :=
+Lemma step_prog_deterministic (prog : program):
+    forall c1 c2 c2' σ1 σ2 σ2',
+      step_prog prog (c1, σ1) (c2, σ2) →
+      step_prog prog (c1, σ1) (c2', σ2') →
+      c2 = c2' ∧ σ2 = σ2'.
+  Proof.
+    intros * H1 H2; split; inversion H1; inversion H2; auto; try congruence.
+  Qed.
+
+(**
+Inductive Exec_prog_step_ind (prog : program) (φ : Conf) : Conf -> Prop :=
 | ExecJmp (w : Word) (r : Register) (PC_is_jmp : prog (PC φ) = Some (Jmp r))
     (reg_is_valid : (reg φ) !! r = Some w) :
-    Exec_prog_one_step_ind prog φ (update_PC φ w)
+    Exec_prog_step_ind prog φ (update_PC φ w)
 | ExecJnzNonZero (w1 w2 : Word) (r1 r2 : Register)
     (PC_is_jnz : prog (PC φ) = Some (Jnz r1 r2))
     (reg1_is_valid : (reg φ) !! r1 = Some w1)
     (reg2_is_valid : (reg φ) !! r2 = Some w2)
     (nz2 : nonZero w2 = true) :
-    Exec_prog_one_step_ind prog φ (update_PC φ w1)
+    Exec_prog_step_ind prog φ (update_PC φ w1)
 | ExecJnzZero (w1 w2 : Word) (r1 r2 : Register)
     (PC_is_jnz : prog (PC φ) = Some (Jnz r1 r2))
     (reg1_is_valid : (reg φ) !! r1 = Some w1)
     (reg2_is_valid : (reg φ) !! r2 = Some w2)
     (nz2 : nonZero w2 = false) :
-    Exec_prog_one_step_ind prog φ (incr_PC φ)
+    Exec_prog_step_ind prog φ (incr_PC φ)
 | ExecLoad (wsrc asrc : Word) (rdst rsrc : Register)
     (PC_is_load : prog (PC φ) = Some (Load rdst rsrc))
     (regsrc_is_valid : (reg φ) !! rsrc = Some wsrc)
     (memwsrc_is_valid : (mem φ) !! wsrc = Some asrc) :
-    Exec_prog_one_step_ind prog φ (incr_PC (update_reg φ rdst asrc))
+    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst asrc))
 | ExecStore (wdst wsrc : Word) (rdst : Register) (ρ : nat + Register)
     (PC_is_store : prog (PC φ) = Some (Store rdst ρ))
     (regdst_is_valid : (reg φ) !! rdst = Some wdst)
     (eq_ρ : word_from_argument φ ρ = Some wsrc) :
-    Exec_prog_one_step_ind prog φ (incr_PC (update_mem φ wdst wsrc))
+    Exec_prog_step_ind prog φ (incr_PC (update_mem φ wdst wsrc))
 | ExecMoveReg (wdst wsrc : Word) (rdst : Register) (ρ : nat + Register)
     (PC_is_mov : prog (PC φ) = Some (Mov rdst ρ))
     (regdst_is_valid : (reg φ) !! rdst = Some wdst)
     (eq : word_from_argument φ ρ = Some wsrc) :
-    Exec_prog_one_step_ind prog φ (incr_PC (update_reg φ wdst wsrc))
+    Exec_prog_step_ind prog φ (incr_PC (update_reg φ wdst wsrc))
 | ExecAdd (wdst w1 w2 : Word) (rdst : Register) (ρ1 ρ2 : nat + Register)
     (PC_is_add : prog (PC φ) = Some (Add rdst ρ1 ρ2))
     (regdst_is_valid : (reg φ) !! rdst = Some wdst)
     (eq_ρ1 : word_from_argument φ ρ1 = Some w1)
     (eq_ρ2 : word_from_argument φ ρ2 = Some w2) :
-    Exec_prog_one_step_ind prog φ (incr_PC (update_reg φ rdst (w1 + w2)))
+    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst (w1 + w2)))
 | ExecSub (wdst w1 w2 : Word) (rdst : Register) (ρ1 ρ2 : nat + Register)
     (PC_is_sub : prog (PC φ) = Some (Sub rdst ρ1 ρ2))
     (regdst_is_valid : (reg φ) !! rdst = Some wdst)
     (eq_ρ1 : word_from_argument φ ρ1 = Some w1)
     (eq_ρ2 : word_from_argument φ ρ2 = Some w2) :
-    Exec_prog_one_step_ind prog φ (incr_PC (update_reg φ rdst (w1 - w2)))
+    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst (w1 - w2)))
 .
+*)
 
-Definition Exec_prog_ind (prog : program) : relation ExecConf :=
-    rtc (Exec_prog_one_step_ind prog).
+Definition steps_prog (prog : program) : relation Conf :=
+    rtc (step_prog prog).
 
-Definition Exec_prog_n_steps_ind (prog : program) : nat -> relation ExecConf :=
-    nsteps (Exec_prog_one_step_ind prog).
+Definition n_steps_prog (prog : program) : nat -> relation Conf :=
+    nsteps (step_prog prog).
 
-Lemma testExec_Prog : Exec_prog_one_step_ind (list_prog_to_prog [Add 0 (inl 1) (inr 0)]) (0, <[0:=0]>(emptyReg), emptyMem) (1, <[0:=1]>(emptyReg), emptyMem).
+Lemma testExec_Prog : step_prog (list_prog_to_prog [Add 0 (inl 1) (inr 0)]) (NextI, (0, <[0:=0]>(emptyReg), emptyMem)) (NextI, (1, <[0:=1]>(emptyReg), emptyMem)).
 Proof.
-    assert (incr_PC (update_reg (0, <[0:=0]>(emptyReg), emptyMem) 0 (1 + 0)) = (1, <[0:=1]>(emptyReg), emptyMem)) as H.
+    assert (exec_instr (Add 0 (inl 1) (inr 0)) (0, <[0:=0]> emptyReg,
+emptyMem)  = (NextI, (1, <[0:=1]>(emptyReg), emptyMem))) as H.
     { reflexivity. }
     rewrite -H.
-    eapply ExecAdd; try reflexivity.
+    apply step_PC_succ.
+    reflexivity.
 Qed.
 
 (**
-Lemma soundness_exec_one_step (prog : program) (φ φ' : ExecConf) :
-    Exec_prog_one_step_ind prog φ φ' -> exec_prog prog φ 1 = φ'.
-    (** The other direction doesn't hold because invalid operation do nothing (configuration stays the same) in exec_prog while in Exec_prog_one_step_ind there is no evaluation step. *)
+Lemma soundness_exec_step (prog : program) (φ φ' : ExecConf) :
+    Exec_prog_step_ind prog φ φ' -> exec_prog prog φ 1 = φ'.
+    (** The other direction doesn't hold because invalid operation do nothing (configuration stays the same) in exec_prog while in Exec_prog_step_ind there is no evaluation step. *)
     (** Find out how to automate this better, the few match statements should be given to crush *)
 Proof.
     intro H.
@@ -260,101 +309,57 @@ Proof.
 Qed.
 *)
 
-Ltac bind_rewrite :=
-    match goal with
-    | H : ?x = _ |- context[?x ≫= _] => rewrite H; cbn
-    end.
-
-Ltac bool_cond_if :=
-    match goal with
-    | H : ?x = true |- context[if ?x then _ else _] => rewrite H
-    | H : ?x = false |- context[if ?x then _ else _] => rewrite H
-    end.
-
-Ltac subst_in_match :=
-    match goal with
-    | H : ?x = _ |- context[match ?x with
-                        | _ => _
-                        end] => rewrite H; cbn
-    end.
 
 
-Lemma soundness_exec_one_step (prog : program) (φ φ' : ExecConf) :
-    Exec_prog_one_step_ind prog φ φ' -> exec_prog prog φ 1 = φ'.
-    (** The other direction doesn't hold because invalid operation do nothing (configuration stays the same) in exec_prog while in Exec_prog_one_step_ind there is no evaluation step. *)
-    (** Find out how to automate this better, the few match statements should be given to crush *)
+Lemma soundness_exec_step (prog : program) (φ : ExecConf) :
+    step_prog prog (NextI, φ) (exec_prog prog φ 1).
 Proof.
-    intro H.
-    destruct (prog (PC φ)) as [i|] eqn:Hi.
-    - induction i; inversion H; try congruence;
-        simpl; unfold exec_instr_at_PC; rewrite Hi; simpl;
-        match goal with
-        | H1 : prog (PC φ) = _, H2 : prog (PC φ) = _ |- _ =>
-            rewrite H1 in H2; injection H2; intros
-        end; subst;
-        repeat bind_rewrite; try bool_cond_if; reflexivity.
-    - inversion H; congruence.
+    unfold exec_prog. unfold exec_step_prog.
+    destruct (prog (PC φ)) as [] eqn:HPC.
+    - (** A lot of work to demonstrate that something is actually an identity function *)
+      destruct (exec_instr i φ) as [] eqn:Hexec.
+      destruct c; rewrite -Hexec; constructor; assumption.
+    - constructor. assumption.
 Qed.
 
-Hint Unfold exec_instr_at_PC : core.
-
-Lemma soundness_exec_n_steps (prog : program) (n : nat) (φ φ' : ExecConf) :
-    Exec_prog_n_steps_ind prog n φ φ' -> exec_prog prog φ n = φ'.
+Lemma exec_step_prog_exec_prog_n_is_exec_prog_Sn (time : nat) (prog : program) (φ : ExecConf) :
+        exec_prog prog φ (S time) =
+        match exec_step_prog prog φ with
+                    | (NextI, φ') => exec_prog prog φ' time
+                    | x => x
+                    end.
 Proof.
-    revert φ φ'.
-    induction n; intros φ φ' H; inversion H; crush.
-    unfold exec_instr_at_PC.
-    apply IHn in H2.
-    inversion H1; subst_in_match;
-    repeat bind_rewrite;
-    crush.
-Qed.
+    simpl. destruct (exec_step_prog prog φ) as [] eqn:Hexec.
+      destruct c; reflexivity.
+Qed. 
 
-Print gmap_lookup.
-
-Ltac check_bounded_partial_map_for_match :=
-    lazymatch goal with
-    | H: context[match ?x !! ?y ≫= _ with
-           | @Some z _ => match type_of z with
-                          | ExecConf => _
-                          | _ => _
-                          end
-           | None => _
-        end] |- _
-        => idtac x; idtac y; destruct (?x !! ?y)
-    end.
-
-Lemma soundness_exec_one_step' (prog : program) (φ φ' : ExecConf) :
-    exec_prog prog φ 1 = φ' -> φ = φ' \/ Exec_prog_one_step_ind prog φ φ'.
+Lemma sum_time_exec_prog (time1 time2 : nat) (prog : program) (φ : ExecConf) :
+    match exec_prog prog φ time1 with
+                    | (NextI, φ') => exec_prog prog φ' time2
+                    | x => x
+                    end
+        = exec_prog prog φ (time1 + time2).
 Proof.
-    simpl. unfold exec_instr_at_PC.
-    destruct (prog (PC φ)) as [] eqn:PCφ.
-    - destruct i as [] eqn:Hi.
-        + unfold exec_instr_opt. intros.
-        Set Printing Existential Instances. Set Printing Implicit. check_bounded_partial_map_for_match. destruct (reg φ !! r) as [|] eqn:Hr; crush.
-          right. eapply ExecJmp; try eauto.
-        
+    revert φ.
+    induction time1.
+    - reflexivity.
+    - simpl. intro φ. destruct (exec_step_prog prog φ) as [] eqn:Hexec.
+      destruct c; auto.
+Qed. 
 
 
-Lemma soundness_exec_n_steps' (prog : program) (n : nat) (φ φ' : ExecConf) :
-    exec_prog prog φ n = φ' -> exists m, m <= n -> Exec_prog_n_steps_ind prog m φ φ'.
+Lemma soundness_exec_n_steps (prog : program) (m : nat) (φ : ExecConf) :
+    exists n, n <= m -> n_steps_prog prog n (NextI, φ) (exec_prog prog φ m).
 Proof.
-    revert φ φ'.
-    induction n; intros φ φ' H.
-    - eexists 0. subst. constructor.
-    - cbn in H. unfold exec_instr_at_PC in H. 
-      destruct (prog (PC φ)); try destruct (exec_instr_opt i φ) as [|] eqn:Hi;
-      try solve[subst; exists 0; constructor].
-      apply IHn in H as [m H].
-      exists (S m). intros HSmSn. apply le_S_n in HSmSn. apply H in HSmSn.
-      econstructor; try exact HSmSn.
-
-      
-    unfold exec_instr_at_PC.
-    apply IHn in H2.
-    inversion H1; subst_in_match;
-    repeat bind_rewrite;
-    crush.
+    revert φ.
+    induction m; intros φ.
+    - exists 0. constructor.
+    - simpl. destruct (exec_step_prog prog φ) as [] eqn:Hexec. specialize (IHm e) as [n H].
+      destruct c; eexists; intro ineq; econstructor; try apply soundness_exec_step.
+      + simpl. rewrite Hexec. constructor.
+      + simpl. rewrite Hexec. constructor.
+      + simpl. rewrite Hexec.
+        apply le_S_n in ineq. apply H. assumption.
 Qed.
 
     
