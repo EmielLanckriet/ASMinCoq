75df44aef9df419fe85e5dc5022acb4488041545
Emiel Lanckriet <emiel.lanckriet@kuleuven.com>
2024-09-04 12:42:17 +0200

    Shown soundness for general asm

--- asmGen.v
+++ asmGen.v
@@ -159,22 +159,17 @@ Definition list_prog_to_prog (li : list instr) : program :=
 Definition exec_step_prog (prog : program) (φ : ExecConf) : Conf :=
     let i := prog (PC φ) in exec_instr i φ.
 
-(** TODO: I stopped here on friday 30th of august *)
 
 Inductive step_prog (prog : program) : Conf -> Conf -> Prop :=
-    | step_PC_fail (φ : ExecConf) (PC_invalid : prog (PC φ) = None) :
-        step_prog prog (NextI, φ) (Failed, φ)
-    | step_PC_succ (φ : ExecConf) (i : instr) (PC_valid : prog (PC φ) = Some i) :
-        step_prog prog (NextI, φ) (exec_instr i φ).
+    | step_PC_i (φ : ExecConf) :
+        step_prog prog (NextI, φ) (exec_instr (prog (PC φ)) φ).
 
-Fixpoint exec_prog (prog : program) (φ : ExecConf) (time : nat) : Conf :=
-    match time with
-    | 0 => (NextI, φ)
-    | S time' => match (exec_step_prog prog φ) with
-                 | (NextI, φ') => exec_prog prog φ' time'
-                 | x => x
-                 end
-    end.
+Lemma estep_PC_i (prog : program) (φ : ExecConf) (c : Conf) (i : instr) (PC_i : prog (PC φ) = i) :
+    c = exec_instr i φ -> step_prog prog (NextI, φ) c.
+Proof.
+    intros. subst. econstructor.
+Qed.
+
 
 Lemma step_prog_deterministic (prog : program):
     forall c1 c2 c2' σ1 σ2 σ2',
@@ -185,159 +180,108 @@ Lemma step_prog_deterministic (prog : program):
     intros * H1 H2; split; inversion H1; inversion H2; auto; try congruence.
   Qed.
 
-(**
-Inductive Exec_prog_step_ind (prog : program) (φ : Conf) : Conf -> Prop :=
-| ExecJmp (w : Word) (r : Register) (PC_is_jmp : prog (PC φ) = Some (Jmp r))
-    (reg_is_valid : (reg φ) !! r = Some w) :
-    Exec_prog_step_ind prog φ (update_PC φ w)
-| ExecJnzNonZero (w1 w2 : Word) (r1 r2 : Register)
-    (PC_is_jnz : prog (PC φ) = Some (Jnz r1 r2))
-    (reg1_is_valid : (reg φ) !! r1 = Some w1)
-    (reg2_is_valid : (reg φ) !! r2 = Some w2)
-    (nz2 : nonZero w2 = true) :
-    Exec_prog_step_ind prog φ (update_PC φ w1)
-| ExecJnzZero (w1 w2 : Word) (r1 r2 : Register)
-    (PC_is_jnz : prog (PC φ) = Some (Jnz r1 r2))
-    (reg1_is_valid : (reg φ) !! r1 = Some w1)
-    (reg2_is_valid : (reg φ) !! r2 = Some w2)
-    (nz2 : nonZero w2 = false) :
-    Exec_prog_step_ind prog φ (incr_PC φ)
-| ExecLoad (wsrc asrc : Word) (rdst rsrc : Register)
-    (PC_is_load : prog (PC φ) = Some (Load rdst rsrc))
-    (regsrc_is_valid : (reg φ) !! rsrc = Some wsrc)
-    (memwsrc_is_valid : (mem φ) !! wsrc = Some asrc) :
-    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst asrc))
-| ExecStore (wdst wsrc : Word) (rdst : Register) (ρ : nat + Register)
-    (PC_is_store : prog (PC φ) = Some (Store rdst ρ))
-    (regdst_is_valid : (reg φ) !! rdst = Some wdst)
-    (eq_ρ : word_from_argument φ ρ = Some wsrc) :
-    Exec_prog_step_ind prog φ (incr_PC (update_mem φ wdst wsrc))
-| ExecMoveReg (wdst wsrc : Word) (rdst : Register) (ρ : nat + Register)
-    (PC_is_mov : prog (PC φ) = Some (Mov rdst ρ))
-    (regdst_is_valid : (reg φ) !! rdst = Some wdst)
-    (eq : word_from_argument φ ρ = Some wsrc) :
-    Exec_prog_step_ind prog φ (incr_PC (update_reg φ wdst wsrc))
-| ExecAdd (wdst w1 w2 : Word) (rdst : Register) (ρ1 ρ2 : nat + Register)
-    (PC_is_add : prog (PC φ) = Some (Add rdst ρ1 ρ2))
-    (regdst_is_valid : (reg φ) !! rdst = Some wdst)
-    (eq_ρ1 : word_from_argument φ ρ1 = Some w1)
-    (eq_ρ2 : word_from_argument φ ρ2 = Some w2) :
-    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst (w1 + w2)))
-| ExecSub (wdst w1 w2 : Word) (rdst : Register) (ρ1 ρ2 : nat + Register)
-    (PC_is_sub : prog (PC φ) = Some (Sub rdst ρ1 ρ2))
-    (regdst_is_valid : (reg φ) !! rdst = Some wdst)
-    (eq_ρ1 : word_from_argument φ ρ1 = Some w1)
-    (eq_ρ2 : word_from_argument φ ρ2 = Some w2) :
-    Exec_prog_step_ind prog φ (incr_PC (update_reg φ rdst (w1 - w2)))
-.
-*)
-
 Definition steps_prog (prog : program) : relation Conf :=
     rtc (step_prog prog).
 
 Definition n_steps_prog (prog : program) : nat -> relation Conf :=
     nsteps (step_prog prog).
 
+Inductive NilView : vec Word 0 -> Set :=
+  nilView : NilView [#].
+
+Inductive ConsView {n : nat} : (vec Word (S n)) -> Set :=
+  consView : forall (w : Word) (v : vec Word n), ConsView (w ::: v).
+
+Definition view {n : nat} (v : vec Word n) :
+  match n with 
+  | 0 => NilView
+  | S n => @ConsView n
+  end v := match v with
+   | [#] => nilView
+   | (v ::: vs) => consView v vs
+end.
+
+Definition binaryOn2Vec {B : Type} (f_bin : Word -> Word -> B) (v : vec Word 2) : B :=
+    match view v with
+    | consView w1 v' => 
+      match view v' with
+      | consView w2 v'' =>
+        match view v'' with
+        | nilView => f_bin w1 w2
+        end
+      end
+    end.
+    
+Definition add_vec_2 := binaryOn2Vec (fun x y => x + y).
+    
+
+Definition Add (dst: Register) (r1 r2: Word + Register) : instr :=
+    Computation [# r1; r2] dst add_vec_2.
+
 Lemma testExec_Prog : step_prog (list_prog_to_prog [Add 0 (inl 1) (inr 0)]) (NextI, (0, <[0:=0]>(emptyReg), emptyMem)) (NextI, (1, <[0:=1]>(emptyReg), emptyMem)).
 Proof.
-    assert (exec_instr (Add 0 (inl 1) (inr 0)) (0, <[0:=0]> emptyReg,
-emptyMem)  = (NextI, (1, <[0:=1]>(emptyReg), emptyMem))) as H.
+    eapply estep_PC_i.
     { reflexivity. }
-    rewrite -H.
-    apply step_PC_succ.
     reflexivity.
 Qed.
 
-(**
-Lemma soundness_exec_step (prog : program) (φ φ' : ExecConf) :
-    Exec_prog_step_ind prog φ φ' -> exec_prog prog φ 1 = φ'.
-    (** The other direction doesn't hold because invalid operation do nothing (configuration stays the same) in exec_prog while in Exec_prog_step_ind there is no evaluation step. *)
-    (** Find out how to automate this better, the few match statements should be given to crush *)
-Proof.
-    intro H.
-    destruct (prog (PC φ)) as [i|] eqn:Hi.
-    - induction i; inversion H; try congruence;
-        simpl; unfold exec_instr_at_PC; rewrite Hi; simpl;
-        match goal with
-        | H1 : prog (PC φ) = _, H2 : prog (PC φ) = _ |- _ =>
-            rewrite H1 in H2; injection H2; intros
-        end;
-        subst;
-        do 2 try match goal with
-        | H1 : word_from_argument _ _ = _ |- _ =>
-            rewrite H1
-        end; simpl;
-        try match goal with
-        | H1 : (reg φ) !! _ = _ |- _ =>
-            rewrite H1
-        end; simpl; try reflexivity;
-        try match goal with
-        | H1 : (reg φ) !! _ = _ |- _ =>
-            rewrite H1
-        end; simpl;
-        try match goal with
-        | H1 : nonZero _ = _ |- _ =>
-            rewrite H1
-        end; try reflexivity;
-        try match goal with
-        | H1 : (mem φ) !! _ = _ |- _ =>
-            rewrite H1
-        end; reflexivity.
-    - inversion H; try congruence.
-Qed.
-*)
+(** Fixpoint exec_prog_output_time (prog : program) (φ : ExecConf) (time : nat) : Conf * {rest : nat | rest <= time}.
+    refine (
+        match time with
+        | 0 => ((NextI, φ) , exist _ 0 _)
+        | S time' => match (exec_step_prog prog φ) with
+                    | (NextI, φ') => let (x, rest') := exec_prog_output_time prog φ' time' in (x, exist _ (proj1_sig rest') _)
+                    | x => (x, exist _ time' _)
+                    end
+        end
+    ); try lia.
+    destruct rest'. simpl. apply Nat.le_le_succ_r. exact l.
+Defined.
 
+
+Definition exec_prog (prog : program) (φ : ExecConf) (time : nat) : Conf  :=
+    fst (exec_prog_output_time prog φ time).
+    *)
+
+Fixpoint exec_prog (prog : program) (φ : ExecConf) (time : nat) : Conf :=
+    match time with
+        | 0 => (NextI, φ)
+        | S time' => match (exec_step_prog prog φ) with
+                    | (NextI, φ') => exec_prog prog φ' time'
+                    | x => x
+                    end
+    end.  
 
 
 Lemma soundness_exec_step (prog : program) (φ : ExecConf) :
     step_prog prog (NextI, φ) (exec_prog prog φ 1).
 Proof.
-    unfold exec_prog. unfold exec_step_prog.
-    destruct (prog (PC φ)) as [] eqn:HPC.
-    - (** A lot of work to demonstrate that something is actually an identity function *)
-      destruct (exec_instr i φ) as [] eqn:Hexec.
-      destruct c; rewrite -Hexec; constructor; assumption.
-    - constructor. assumption.
+    eapply estep_PC_i.
+    { reflexivity. }
+    unfold exec_prog. simpl. unfold exec_step_prog.
+    destruct (prog (PC φ)) as [] eqn:HPC; try reflexivity.
+    simpl.
+    destruct (f_condition (inputs_from_inputnatregs (reg φ) inputs)); reflexivity.
 Qed.
 
-Lemma exec_step_prog_exec_prog_n_is_exec_prog_Sn (time : nat) (prog : program) (φ : ExecConf) :
-        exec_prog prog φ (S time) =
-        match exec_step_prog prog φ with
-                    | (NextI, φ') => exec_prog prog φ' time
-                    | x => x
-                    end.
+Lemma soundness_exec (prog : program) (φ : ExecConf) :
+    forall time c, c = exec_prog prog φ time ->
+    steps_prog prog (NextI, φ) c.
 Proof.
-    simpl. destruct (exec_step_prog prog φ) as [] eqn:Hexec.
-      destruct c; reflexivity.
-Qed. 
-
-Lemma sum_time_exec_prog (time1 time2 : nat) (prog : program) (φ : ExecConf) :
-    match exec_prog prog φ time1 with
-                    | (NextI, φ') => exec_prog prog φ' time2
-                    | x => x
-                    end
-        = exec_prog prog φ (time1 + time2).
-Proof.
-    revert φ.
-    induction time1.
-    - reflexivity.
-    - simpl. intro φ. destruct (exec_step_prog prog φ) as [] eqn:Hexec.
-      destruct c; auto.
-Qed. 
-
-
-Lemma soundness_exec_n_steps (prog : program) (m : nat) (φ : ExecConf) :
-    exists n, n <= m -> n_steps_prog prog n (NextI, φ) (exec_prog prog φ m).
-Proof.
-    revert φ.
-    induction m; intros φ.
-    - exists 0. constructor.
-    - simpl. destruct (exec_step_prog prog φ) as [] eqn:Hexec. specialize (IHm e) as [n H].
-      destruct c; eexists; intro ineq; econstructor; try apply soundness_exec_step.
-      + simpl. rewrite Hexec. constructor.
-      + simpl. rewrite Hexec. constructor.
-      + simpl. rewrite Hexec.
-        apply le_S_n in ineq. apply H. assumption.
+    intro time.
+    revert φ.    
+    induction time; intros φ.
+    - simpl. intros. subst. constructor.
+    - simpl. destruct (exec_step_prog prog φ) as [] eqn:Hexec. intros.
+      destruct c.
+      + subst.
+        econstructor.
+        * constructor.
+        * rewrite -Hexec. unfold exec_step_prog. constructor.
+      + subst.
+        econstructor.
+        * constructor.
+        * unfold exec_step_prog in Hexec. rewrite Hexec. apply IHtime. reflexivity.
 Qed.
 
     
